327303aa0d24137c919f6f4a4d1228f4a7e474dc
ac97nj <ac97nj@gmail.com>
2020-09-17 11:42:16 +0800

    Nav全局组件注册

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <router-view/>
-    <router-link to="/Money">明细</router-link>
-    |
-    <router-link to="/Labels">记账</router-link>
-    |
-    <router-link to="/Statistics">统计</router-link>
   </div>
 </template>
 
---
+++ src/components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-link to="/Money">明细</router-link>
+    |
+    <router-link to="/Labels">记账</router-link>
+    |
+    <router-link to="/Statistics">统计</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Nav extends Vue {
+
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
