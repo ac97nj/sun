5f5f09effeaa4d72abf6bc92458ae33a3fd5dde6
ac97nj <ac97nj@gmail.com>
2020-09-17 16:33:50 +0800

    将 SVG 样式分装到 icon 中

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,26 +1,23 @@
 <template>
   <div>
+    <Icon name="label"/>
     <router-link to="/Money">明细</router-link>
     |
+    <Icon name="tally"/>
     <router-link to="/Labels">记账</router-link>
     |
+    <Icon name="sta"/>
     <router-link to="/Statistics">统计</router-link>
   </div>
 </template>
 
 <script lang="ts">
 
-const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {
-  importAll(require.context('../assets/icons', true, /\.svg$/));
-} catch (error) {
-  console.log(error);
-}
 
 
 
-import sun from '../assets/icons/sun.svg'
-console.log(sun)
+
+
 
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
