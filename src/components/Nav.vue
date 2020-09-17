3f2f6ec502f87a4e26fbda208aa7d8555e9f0867
ac97nj <ac97nj@gmail.com>
2020-09-17 15:35:43 +0800

    将所有的 svg 图片进行封装 统一引入 Nav 文件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,6 +9,16 @@
 </template>
 
 <script lang="ts">
+
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
+
+
 import sun from '../assets/icons/sun.svg'
 console.log(sun)
 
