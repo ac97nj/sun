e95e0c9945b8216630d3d7151455dc7d3311f609
ac97nj <ac97nj@gmail.com>
2020-09-17 11:30:49 +0800

    3 个页面

---
+++ src/views/Labels.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>2</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Labels extends Vue {
+
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Money.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>1</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Money extends Vue {
+
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
